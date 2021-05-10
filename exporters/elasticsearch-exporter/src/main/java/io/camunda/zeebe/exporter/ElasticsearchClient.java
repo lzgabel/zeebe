c948b425f44cc16fae40784c54cd7776e68aaf53
李銍 <lizhi@shuwen.com>
2021-05-10 16:55:09 +0800

    fix: 索引按月创建

--- exporters/elasticsearch-exporter/src/main/java/io/camunda/zeebe/exporter/ElasticsearchClient.java
+++ exporters/elasticsearch-exporter/src/main/java/io/camunda/zeebe/exporter/ElasticsearchClient.java
@@ -74,7 +74,7 @@ public class ElasticsearchClient {
     this.log = log;
     client = createClient();
     this.bulkRequest = bulkRequest;
-    formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneOffset.UTC);
+    formatter = DateTimeFormatter.ofPattern("yyyy-MM").withZone(ZoneOffset.UTC);
   }
 
   public void close() throws IOException {
